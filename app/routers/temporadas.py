5fdfda148a8edaaa3c37cfc16e88c5923972705d
BaldurDev <kevin.pizarrosanz@gmail.com>
2025-07-22 16:45:24 -0400

    Update temporadas.py

--- app/routers/temporadas.py
+++ app/routers/temporadas.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, HTTPException, Query
-import json
-from pathlib import Path
 from app.models import Temporada
 from typing import List, Optional
+from app.data.data_manager import DataManager
 
 router = APIRouter(
     prefix="/temporadas",
     tags=["Temporadas"]
 )
 
-DATA_PATH = Path(__file__).resolve().parent.parent / "data" / "temporadas.json"
+data_manager = DataManager()
+
 
 @router.get("/", response_model=List[Temporada])
 def obtener_temporadas(
@@ -18,18 +18,19 @@ def obtener_temporadas(
     anio_estreno: Optional[int] = None,
     numero_temporada: Optional[int] = None
 ):
-    with open(DATA_PATH, "r", encoding="utf-8") as file:
-        data = json.load(file)
+    data = data_manager.get_data("temporadas")
+
     if anio_estreno:
         data = [t for t in data if t["anio_estreno"] == anio_estreno]
     if numero_temporada:
         data = [t for t in data if t["numero_temporada"] == numero_temporada]
-    return data[skip:skip+limit]
+
+    return data[skip:skip + limit]
+
 
 @router.get("/{id}", response_model=Temporada)
 def obtener_temporada_por_id(id: str):
-    with open(DATA_PATH, "r", encoding="utf-8") as file:
-        data = json.load(file)
+    data = data_manager.get_data("temporadas")
     for temporada in data:
         if temporada["id"] == id:
             return temporada
