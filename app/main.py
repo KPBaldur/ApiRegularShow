a3f591f2ded1c6ab8ae142bbb6af6665bec0765d
BaldurDev <kevin.pizarrosanz@gmail.com>
2025-07-22 16:44:49 -0400

    Configuracion de imagen para los personajes
    Se configura una Imagen temporal para los personajes.
    
    Se actualizara pronto para los comics.

--- app/main.py
+++ app/main.py
@@ -1,23 +1,41 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import personajes, capitulos, temporadas, comics
+from app.errors import configure_error_handlers
+from app.config import settings
 
 app = FastAPI(
-    title="Regular Show API",
+    title=settings.app_name,
     description="API pública para obtener información de la serie Regular Show, incluyendo personajes, capítulos, temporadas y cómics.",
     version="1.0.0",
     contact={
         "name": "Kevin P.",
-        "url": "https://github.com/KevinP-Antartica/ApiRegularShow",
-        "email": "kevinp@example.com"
+        "url": "https://github.com/KPBaldur/ApiRegularShow",
+        "email": settings.admin_email
     },
     license_info={
         "name": "MIT License",
         "url": "https://opensource.org/licenses/MIT"
     },
-    terms_of_service="https://github.com/KevinP-Antartica/ApiRegularShow#terms"
+    terms_of_service="https://github.com/KPBaldur/ApiRegularShow#terms"
 )
 
+# Routers
 app.include_router(personajes.router)
 app.include_router(capitulos.router)
 app.include_router(temporadas.router)
 app.include_router(comics.router)
+
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Global Errors
+configure_error_handlers(app)
