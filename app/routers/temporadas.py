47ca3fb310b16bdb0f0d1c0ae7827d8dea41c9f7
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2025-08-03 21:42:59 -0400

    Temporadas
    Se arregla el uso de temporada.
    Se agrega mas endpoints.

--- app/routers/temporadas.py
+++ app/routers/temporadas.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, HTTPException, Query
-from app.models import Temporada
+from app.models import Capitulo, Temporada
 from typing import List, Optional
 from app.data.data_manager import DataManager
+from app.data import data_manager
 
 router = APIRouter(
     prefix="/temporadas",
@@ -10,6 +11,28 @@ router = APIRouter(
 
 data_manager = DataManager()
 
+@router.get("/capitulos-por-temporada", response_model=List[dict])
+def capitulos_por_temporada():
+    temporadas = data_manager.get_data("temporadas")
+    capitulos = data_manager.get_data("capitulos")
+    resultado = []
+    for temporada in temporadas:
+        numero_temporada = temporada["numero_temporada"]
+        capitulos_temp = [c for c in capitulos if c["numero_temporada"] == numero_temporada]
+        resultado.append({
+            "temporada": numero_temporada,
+            "total_capitulos": len(capitulos_temp),
+            "nombres_capitulos": [c["nombre"] for c in capitulos_temp]
+        })
+    return resultado
+
+@router.get("/top-imdb", response_model=List[Capitulo])
+def top_10_capitulos_imdb():
+    capitulos = data_manager.get_data("capitulos")
+    capitulos_ordenados = sorted(
+        capitulos, key=lambda c: c.get("imbdb_score", 0), reverse=True
+    )
+    return capitulos_ordenados[:10]
 
 @router.get("/", response_model=List[Temporada])
 def obtener_temporadas(
