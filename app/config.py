e21a648533bc0e541525c5683981fb62131d37e5
BaldurDev <kevin.pizarrosanz@gmail.com>
2025-08-12 12:27:51 -0400

    Config de instancia Debug y Cors

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
 from pydantic_settings import BaseSettings
+from typing import List
 
 class Settings(BaseSettings):
     app_name: str = "Regular Show API"
     admin_email: str = "kevin.pizarrosanz@gmail.com"
+    debug: bool = False
+    allowed_origins: List[str] = ["*"]
 
     class Config:
         env_file = ".env"
